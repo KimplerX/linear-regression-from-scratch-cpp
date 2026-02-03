20b2f20a5a3b57aff093e9ec4a680f6ef604d681
KimplerX <bedei.mykhailo@student.uzhnu.edu.ua>
2026-02-03 19:03:44 +0200

    I automated the build using build.bat

---
+++ build.bat
@@ -0,0 +1,23 @@
+@echo off
+echo ==========================================
+echo      Starting Build Process...
+echo ==========================================
+
+if not exist build mkdir build
+cd build
+cmake .. -G "MinGW Makefiles"
+cmake --build .
+
+if %errorlevel% neq 0 (
+    echo.
+    echo [ERROR] Build failed! Fix errors and try again.
+    pause
+    exit /b %errorlevel%
+)
+
+echo.
+echo [SUCCESS] Build complete. Running program...
+echo ==========================================
+echo.
+
+pause
